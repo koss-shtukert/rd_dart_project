663c0b483ac499c6436058058be2b08400910ecc
Kostiantyn Shtukert <k.shtukert@foodtech.team>
2026-03-10 11:18:04 +0200

    Initial commit

---
+++ bin/rd_dart_project.dart
@@ -0,0 +1,5 @@
+import 'package:rd_dart_project/rd_dart_project.dart' as rd_dart_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rd_dart_project.calculate()}!');
+}
---
+++ test/rd_dart_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:rd_dart_project/rd_dart_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
