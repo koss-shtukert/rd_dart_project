b574cc3ac413547a326e0865bea5ac3d6f80b66a
Kostiantyn Shtukert <k.shtukert@foodtech.team>
2026-03-17 10:33:39 +0200

    Add homework - task5

--- lib/lesson_05/homework.dart
+++ lib/lesson_05/homework.dart
@@ -108,7 +108,19 @@ void task4() {
 /// Поділіть бали на кількість запитань.
 /// Виведіть остаточний результат.
 
-void task5() {}
+void task5() {
+  var score = 0;
+  final correctAnswers = 17;
+  final mistakes = 3;
+  final totalQuestions = 20;
+
+  score += correctAnswers * 10;
+  score -= mistakes * 5;
+  score *= 2;
+  score ~/= totalQuestions;
+
+  print('Score: $score');
+}
 
 /// Завдання 6: Умовні конструкції if/else
 /// Створіть змінну examScore з значенням 56
